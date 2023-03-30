28c016bd99a368940d61e38ea6a08075e8279506
phuong23401 <hoaphuong23401@gmail.com>
2023-03-30 20:52:26 +0700

    Update: code.

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ const MOSJS = class {
 
     static async getNFTIdentifierMode({ nodeAddress, contractPackageHash, networkName, middlewareAPI }) {
         const stateRootHash = await utils.getStateRootHash(nodeAddress)
-        let data = await axios(`${middlewareAPI}state_root_hash=${stateRootHash}&key=hash-${contractPackageHash}`)
+        let data = await axios(`${middlewareAPI}?state_root_hash=${stateRootHash}&key=hash-${contractPackageHash}`)
         data = data.data.result.stored_value
         let namedKeys = []
         let entryPoints = []
@@ -255,7 +255,7 @@ const MOSJS = class {
             })
             retPackageHash = contractPackageHash
             const activeContractHash = lastVersion.contract_hash.substring("contract-".length)
-            let activeContractData = await axios(`${middlewareAPI}state_root_hash=${stateRootHash}&key=hash-${activeContractHash}`)
+            let activeContractData = await axios(`${middlewareAPI}?state_root_hash=${stateRootHash}&key=hash-${activeContractHash}`)
             activeContractData = activeContractData.data.result.stored_value.Contract
             namedKeys = activeContractData.named_keys
             entryPoints = activeContractData.entry_points
